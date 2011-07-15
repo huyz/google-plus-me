912c95c54a2a6cdd708b355508d37ce5579c40c4
huyz <h-git@huyz.us>
2011-07-16 01:15:11 +1000

    Fixed zip for Web Store

--- prepare-for-package.sh
+++ prepare-for-package.sh
@@ -3,7 +3,8 @@
 # Package files for distribution as CRX and for Chrome Web Store
 
 [ -d google-plus-me ] || mkdir google-plus-me
-cp -a *.* icons google-plus-me
+cp -a *.html *.js *.css *.json icons google-plus-me
 
 cd google-plus-me || exit 1
+rm -f ../google-plus-me.zip
 zip -r ../google-plus-me *
