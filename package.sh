f5c2c250130d12f345f3ab67b6781a14f15859d5
huyz <h-git@huyz.us>
2011-07-21 19:28:05 +1000

    Zip up beta version

--- package.sh
+++ package.sh
@@ -9,3 +9,6 @@ cp -a *.html *.js *.css *.json icons images dist/
 cd dist || exit 1
 rm -f ../google-plus-me.zip
 zip -r ../google-plus-me *
+
+perl -pi -e 's/G\+me\b/G+me (BETA)/; s/"description"\s*:\s*"/$&(BETA) /' manifest.json
+zip -r ../google-plus-me-beta *
