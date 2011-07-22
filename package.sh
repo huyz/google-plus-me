b51e4f8a75164da4e98f0c76088a5625fb6f2fa6
huyz <h-git@huyz.us>
2011-07-22 13:57:03 +1000

    Fixed beta packaging for web store

--- package.sh
+++ package.sh
@@ -7,8 +7,15 @@ mkdir dist || exit 1
 cp -a *.html *.js *.css *.json icons images dist/
 
 cd dist || exit 1
+
+# Web store regular
 rm -f ../google-plus-me.zip
 zip -r ../google-plus-me *
 
-perl -pi -e 's/G\+me\b/G+me (BETA)/; s/"description"\s*:\s*"/$&(BETA) /; s#"homepage_url"#"update_url": "http://huyz.us/gpme-beta-updates.xml",\n  $&#; ' manifest.json
+# Web Store beta
+perl -pi -e 's/G\+me\b/G+me (BETA)/; s/"description"\s*:\s*"/$&(BETA) /;' manifest.json
+rm -f ../google-plus-me-beta.zip
 zip -r ../google-plus-me-beta *
+
+# huyz.us beta
+perl -pi -e 's#"homepage_url"#"update_url": "http://huyz.us/gpme-beta-updates.xml",\n  $&#; ' manifest.json
