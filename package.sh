314f91b42aa83bfe50af978c5ee91424c3911f1f
huyz <h-git@huyz.us>
2011-07-24 19:22:25 +1000

    Keep zip files for every version

--- package.sh
+++ package.sh
@@ -2,20 +2,28 @@
 # huyz 2011-07-14
 # Package files for distribution as CRX and for Chrome Web Store
 
-[ -d dist ] && rm -rf dist
-mkdir dist || exit 1
-cp -a *.html *.js *.css *.json icons images dist/
-
+if [ ! -d dist ]; then
+  mkdir dist || exit 1
+fi
 cd dist || exit 1
+rm -rf google-plus-me
+
+mkdir google-plus-me || exit 1
+cd google-plus-me || exit 1
+
+cp -a ../../{*.{html,js,css,json},icons,images} .
+
+version=$(sed -n 's/.*"version" *: *"\([0-9\.][0-9\.]*\)".*/\1/p' manifest.json)
+[ -n "$version" ] || exit 2
 
 # Web store regular
-rm -f ../google-plus-me.zip
-zip -r ../google-plus-me *
+rm -f ../google-plus-me-$version.zip
+zip -r ../google-plus-me-$version.zip *
 
 # Web Store beta
 perl -pi -e 's/G\+me\b/G+me (BETA)/; s/"description"\s*:\s*"/$&(BETA) /;' manifest.json
-rm -f ../google-plus-me-beta.zip
-zip -r ../google-plus-me-beta *
+rm -f ../google-plus-me-$version-beta.zip
+zip -r ../google-plus-me-$version-beta.zip *
 
 # huyz.us beta
 perl -pi -e 's#"homepage_url"#"update_url": "http://huyz.us/gpme-beta-updates.xml",\n  $&#; ' manifest.json
