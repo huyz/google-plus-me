3ccf0a3ba9a82e7ca6afe074d312804075a97db4
huyz <h-git@huyz.us>
2011-08-14 22:54:00 +1000

    Updated language names

--- fancy-settings/source/manifest.js
+++ fancy-settings/source/manifest.js
@@ -220,27 +220,27 @@ this.manifest = {
       "type"  : "description",
       "text"  : "\
 <table>\
-<tr><td style=\"width: 20%\">Chinese &mdash; traditional</td>\
+<tr><td style=\"width: 35%\">中文 (繁體) - Chinese (Traditional)</td>\
 <td><a href=\"https://plus.google.com/111120240496403650720\" target=_blank>陳彥廷</a></td></tr>\
-<tr><td>Deutsch</td>\
-<td><a href=\"https://plus.google.com/111611377225917333080\" target=_blank>Nick Rock</a></td></tr>\
-<tr><td>Dutch</td>\
-<td><a href=\"https://plus.google.com/105310358762146417461\" target=_blank>Peter Klein</a></td></tr>\
-<tr><td>Español AL</td>\
-<td><a href=\"https://plus.google.com/103470335401117697610\" target=_blank>Andrés D Bevilacqua (aka PaiSand)</a></td></tr>\
-<tr><td>Français</td>\
-<td><a href=\"https://plus.google.com/107140639929094144668\" target=_blank>Olivier de Broqueville</a>, <a href=\"https://plus.google.com/100633553439138235276\" target=_blank>Eddy Deligne</a></td></tr>\
-<tr><td>Hungarian</td>\
-<td><a href=\"https://plus.google.com/108092012091902688220\" target=_blank>Zsolt Dollenstein</a></td></tr>\
-<tr><td>Indonesian</td>\
+<tr><td>日本語 - Japanese</td>\
+<td><a href=\"https://plus.google.com/104214201726373605313\" target=_blank>Toshitsugu Miyoshi</a></td></tr>\
+<tr><td>Bahasa Indonesia</td>\
 <td><a href=\"https://plus.google.com/104214201726373605313\" target=_blank>Teddy Yuliustanto</a></td></tr>\
+<tr><td>Deutsch - German</td>\
+<td><a href=\"https://plus.google.com/111611377225917333080\" target=_blank>Nick Rock</a></td></tr>\
+<tr><td>Español (Latinoamérica) - Spanish</td>\
+<td><a href=\"https://plus.google.com/103470335401117697610\" target=_blank>Andrés D Bevilacqua (aka PaiSand)</a></td></tr>\
+<tr><td>Français - French</td>\
+<td><a href=\"https://plus.google.com/107140639929094144668\" target=_blank>Olivier de Broqueville</a>, <a href=\"https://plus.google.com/100633553439138235276\" target=_blank>Eddy Deligne</a></td></tr>\
 <tr><td>Italiano</td>\
 <td><a href=\"https://plus.google.com/108442963639756243080\" target=_blank>Marco Mililotti</a></td></tr>\
-<tr><td>Japanese</td>\
-<td><a href=\"https://plus.google.com/104214201726373605313\" target=_blank>Toshitsugu Miyoshi</a></td></tr>\
-<tr><td>Portuguese BR</td>\
+<tr><td>Magyar - Hungarian</td>\
+<td><a href=\"https://plus.google.com/108092012091902688220\" target=_blank>Zsolt Dollenstein</a></td></tr>\
+<tr><td>Nederlands - Dutch</td>\
+<td><a href=\"https://plus.google.com/105310358762146417461\" target=_blank>Peter Klein</a></td></tr>\
+<tr><td>Português (Brasil)</td>\
 <td><a href=\"https://plus.google.com/117755969537089366397\" target=_blank>Cesar JB</a></td></tr>\
-<tr><td>Portuguese PT</td>\
+<tr><td>Português (Portugal)</td>\
 <td><a href=\"https://plus.google.com/102936041566944563648\" target=_blank>Pedro Ricardo</a></td></tr>\
 </table>\
         "
